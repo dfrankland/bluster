b1603c5d345fa9a856ec5caa0c25a8e701734b92
Dylan Frankland <dylan@frankland.io>
2019-03-24 19:48:31 -0700

    Silence warnings for ATOMIC_USIZE_INIT deprecation
    See https://github.com/SSheldon/rust-objc/pull/75 for updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// See https://github.com/SSheldon/rust-objc/pull/75 for updates on issues to do with compiler
+// warnings caused by `ATOMIC_USIZE_INIT` being deprecated
+#![allow(deprecated)]
+
 mod error;
 pub mod gatt;
 mod peripheral;
